cef60f5ba5ad6208edf8b981b9a8551a69e38546
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-07-24 18:12:35 +0200

    new UI

--- slider.js
+++ slider.js
@@ -3,7 +3,7 @@ const sliderTrackWidth = sliderTrack.offsetWidth;
 const opinions = Array.from(sliderTrack.children);
 
 const opinion = document.querySelector('.slider__card');
-const opinionWidth = opinion.offsetWidth + 16;
+const opinionWidth = opinion.offsetWidth;
 
 const allOpinionCards = document.querySelectorAll('.slider__card');
 const totalOpinionCards = allOpinionCards.length;
@@ -13,14 +13,15 @@ const totalSliderWidth = totalOpinionCards * opinionWidth;
 
 const prevBtn = document.getElementById('btn__prev');
 prevBtn.addEventListener('click', () => {
-    sliderTrack.scrollLeft -= opinionWidth;
+  sliderTrack.scrollLeft -= opinionWidth;
 });
 
 const nextBtn = document.getElementById('btn__next');
 nextBtn.addEventListener('click', () => {
-    sliderTrack.scrollLeft += opinionWidth;
+  sliderTrack.scrollLeft += opinionWidth;
 });
 
 // const autoScroller = setInterval(() => {
 //     slider.scrollLeft += opinionsWidth;
 // }, 5000);
+
